f8568967b63f1b3e43cbf53c87b14dcd94f5c228
kodelittle <kode.little@gmail.com>
2015-12-25 22:52:44 +0530

    Failing test. Successful check in?

---
+++ src/test/java/kode/little/java/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package kode.little.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
